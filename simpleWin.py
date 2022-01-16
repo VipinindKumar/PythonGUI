31561ad5e3d5385b85c7ac9cefdf1d7fd5b78df4
Vipin Kumar <vipin.ind.kumar@gmail.com>
2022-01-16 17:01:30 +0530

    add button tht can be clicked

--- simpleWin.py
+++ simpleWin.py
@@ -7,8 +7,20 @@ win.title("Simple Pyhton GUI window")
 # win.resizable(0, 0)
 
 # adding label
-from tkinter import ttk
+from tkinter import ttk # themed tk
 
-ttk.Label(win, text="Aye Hello").grid(column=0, row=0)
+lable1 = ttk.Label(win, text="Aye Hello")
+lable1.grid(column=0, row=0)
+
+# button click event callback fun
+def click():
+	action.configure(text="## Clicked ##")
+	action.configure(foreground="red")
+
+# Creating buttons
+action = ttk.button(win, text="Click here", command=click)
+action.grid(column=1, row=0)
+
+
 
 win.mainloop()
