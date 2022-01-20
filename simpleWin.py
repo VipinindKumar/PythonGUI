1d99e2fc2342c71405d786f9e372855006ffcf4c
Vipin Kumar <vipin.ind.kumar@gmail.com>
2022-01-20 12:07:12 +0530

    code text widget with button

--- simpleWin.py
+++ simpleWin.py
@@ -9,17 +9,29 @@ win.title("Simple Pyhton GUI window")
 # adding label
 from tkinter import ttk # themed tk
 
-lable1 = ttk.Label(win, text="Aye Hello")
-lable1.grid(column=0, row=0)
+# lable1 = ttk.Label(win, text="Aye Hello")
+# lable1.grid(column=0, row=0)
 
-# button click event callback fun
+# # button click event callback fun
+# def click():
+# 	button1.configure(text="## Clicked ##")
+# 	lable1.configure(foreground="red")
+
+# button click function
 def click():
-	button1.configure(text="## Clicked ##")
-	lable1.configure(foreground="red")
+	button1.configure(text='Hello ' + name.get())
 
 # Creating buttons
 button1 = ttk.Button(win, text="Click here", command=click)
-button1.grid(column=1, row=0)
+button1.grid(column=1, row=1)
+
+# lable
+ttk.Label(win, text='Enter your name:').grid(column=0, row=0)
+
+# adding a text box widget
+name = tk.StringVar()
+nameEntered = ttk.Entry(win, width=12, textvariable=name)
+nameEntered.grid(column=0, row=1)
 
 
 
